43cbeb1f775db962f52a9d09d1fce31410016ac9
claire <vitsingz@gmail.com>
2025-03-30 22:56:55 +0200

    dockeriser le projet

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -16,31 +16,31 @@ class Author
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['author:read', 'book:read','category:read'])]
+    #[Groups(['author:read', 'book:read', 'category:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
-    #[Groups(['author:read', 'book:read','category:read'])]
+    #[Groups(['author:read', 'book:read', 'category:read'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 100)]
-    #[Groups(['author:read', 'book:read','category:read'])]
+    #[Groups(['author:read', 'book:read', 'category:read'])]
     private ?string $lastName = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Groups(['author:read', 'book:read','category:read'])]
+    #[Groups(['author:read', 'book:read', 'category:read'])]
     private ?\DateTimeInterface $birthDate = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups(['author:read', 'book:read','category:read'])]
+    #[Groups(['author:read', 'book:read', 'category:read'])]
     private ?string $biography = null;
 
     /**
      * @var Collection<int, Book>
      */
-    #[ORM\ManyToMany(targetEntity: Book::class, inversedBy:'authors', cascade: ['persist','remove'], orphanRemoval: true)]
+    #[ORM\ManyToMany(targetEntity: Book::class, inversedBy: 'authors', cascade: ['persist', 'remove'], orphanRemoval: true)]
     #[Groups(['author:read'])]
-    #[ORM\JoinTable(name: 'Author_Book')]
+    #[ORM\JoinTable(name: 'author_book')]
     #[ORM\JoinColumn(name: 'author_id', referencedColumnName: 'id')]
     #[ORM\InverseJoinColumn(name: 'book_id', referencedColumnName: 'id')]
     #[MaxDepth(1)]
